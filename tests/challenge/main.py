fd4f8b27ed1279d3eb8e0ff510cf90d1d98869bf
Lucas <lucas_cleminson@eticalgarve.com>
2026-04-22 20:42:57 +0000

    feat: aula de pytest

---
+++ tests/challenge/main.py
@@ -0,0 +1,5 @@
+from challenge.controlador import Controlador
+
+
+if __name__ == "__main__":
+    Controlador()
---
+++ tests/tutorial/test_main.py
@@ -0,0 +1,7 @@
+from main import soma
+
+def test_soma():
+    assert soma(2, 3) == 6
+    
+if __name__ == "__main__":
+    test_soma()
